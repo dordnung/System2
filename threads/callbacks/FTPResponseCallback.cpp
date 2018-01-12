475ed4f1378656f59fdb51b3917941e31f865883
David Ordnung <git@dordnung.de>
2018-01-13 00:47:50 +0100

    Add FTPResponseCallback

---
+++ threads/callbacks/FTPResponseCallback.cpp
@@ -0,0 +1,39 @@
+/**
+ * -----------------------------------------------------
+ * File        FTPResponseCallback.cpp
+ * Authors     David Ordnung
+ * License     GPLv3
+ * Web         http://dordnung.de
+ * -----------------------------------------------------
+ *
+ * Copyright (C) 2013-2018 David Ordnung
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>
+ */
+
+#include "FTPResponseCallback.h"
+
+
+FTPResponseCallback::FTPResponseCallback(FTPRequest *ftpRequest, std::string error, bool isDownload)
+    : ResponseCallback(ftpRequest, error), isDownload(isDownload) {}
+
+
+FTPResponseCallback::FTPResponseCallback(FTPRequest *ftpRequest, CURL *curl, std::string content, bool isDownload)
+    : ResponseCallback(ftpRequest, curl, content), isDownload(isDownload) {}
+
+
+void FTPResponseCallback::PreFire() {
+    // Push whether something was downloaded or uploaded
+    this->request->responseCallback->PushCell(this->isDownload);
+}
