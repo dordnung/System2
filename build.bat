112738e110f48619fd851aac5d435911f3affc9e
David Ordnung <git@dordnung.de>
2020-06-05 01:00:54 +0200

    Fix Windows build

--- build.bat
+++ build.bat
@@ -1,8 +1,7 @@
 set SYSTEM2_DIR=%cd%
 set BUILD_DIR=%SYSTEM2_DIR%\build-windows
-set VCVARSALL="C:\Program Files (x86)\Microsoft Visual Studio\2019\VC\Auxiliary\Build\vcvarsall.bat"
-dir "C:\Program Files (x86)\Microsoft Visual Studio\2019"
-dir "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools"
+set VCVARSALL="C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\VC\Auxiliary\Build\vcvarsall.bat"
+dir "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\VC"
 
 if not exist "%BUILD_DIR%" (
 	mkdir "%BUILD_DIR%"
